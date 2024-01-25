485e74cb9100c0ae73d7688fafeeffeda44bfd86
LAPTOP-JFR3B5TB\yael_ <dariocalvario@udelp.edu.mx>
2024-01-25 08:57:50 -0600

    ObjetoCuenta

--- src/udelp/edu/poo/model/Cuenta.java
+++ src/udelp/edu/poo/model/Cuenta.java
@@ -1,5 +1,39 @@
 package udelp.edu.poo.model;
 
 public class Cuenta {
+	
+	private String titular;
+	
+	private Double cantidad;
+	
+	public  Cuenta(String titular, Double cantidad) {
+		this.cantidad=cantidad;
+		this.titular=titular;
+	}
+	
+	public  Cuenta(String titular) {
+		this.titular=titular;
+		this.cantidad=0.0;
+	}
 
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String toString() {
+		return "Cuenta [titular=" + titular + ", cantidad=" + cantidad + "]";
+	}
+	
 }
